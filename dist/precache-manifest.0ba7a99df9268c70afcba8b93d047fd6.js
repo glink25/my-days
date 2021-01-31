43a19b64b66e2c86ff6f01d412a772cd4d265257
root <root@guoli-PC.localdomain>
2021-01-31 16:59:06 +0800

    feat: add icon files

---
+++ dist/precache-manifest.0ba7a99df9268c70afcba8b93d047fd6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88a8550b1d52c2c64214",
+    "url": "/css/app.b6a5b87a.css"
+  },
+  {
+    "revision": "0258e1d94cd051e1e3ba",
+    "url": "/css/chunk-vendors.77544790.css"
+  },
+  {
+    "revision": "b463699dfdda0ad7831d1a19ede73c4e",
+    "url": "/img/splash-640x920.png"
+  },
+  {
+    "revision": "13461497b3b39d68556689a6e29152bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88a8550b1d52c2c64214",
+    "url": "/js/app.5897e09b.js"
+  },
+  {
+    "revision": "0258e1d94cd051e1e3ba",
+    "url": "/js/chunk-vendors.e30653b3.js"
+  },
+  {
+    "revision": "ad824964bc93275634f434ef98e7690d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
